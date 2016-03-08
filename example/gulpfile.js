61e62e11473d69422f4f48ae78aa38fe7113d245
Trevor <trevorhreed@users.noreply.github.com>
2016-03-08 14:52:29 -0700

    Update gulpfile.js

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,16 +1,6 @@
 require('ng-guzzle')({
-  app: {
-    id: 'app',
-    src: ['./src/app/'],
-    static: ['./src/static/**/*'],
-    dest: './dist/',
-    port: 3011
-  },
-  libs: [
-    {
-      id: 'mde',
-      src: './src/lib/',
-      dest: './dist/'
-    }
-  ]
+  id: 'app',
+  src: ['./src/app/'],
+  static: ['./src/static/**/*'],
+  dest: './dist/'
 });
